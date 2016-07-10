ee029664f4a7fce92683a2403f7f4a4474247d1c
Souleymane MATO <smato@peopleinput.com>
2016-07-10 16:43:07 +0000

    add bookmarks database models

--- bookmarks/models.py
+++ bookmarks/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Link(models.Model):
+    url = models.URLField(unique=True)
+
+class Users(models.Model):
+    username = models.TextField(max_length=255)
+    password = models.CharField(max_length=30)
+    email = models.EmailField()
+
+class Bookmark(models.Model):
+    title = models.CharField(max_length=200)
+    user = models.ForeignKey(Users)
+    link = models.ForeignKey(Link)
