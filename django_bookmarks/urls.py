ffe9959fcf20be0499fd022531f05058611ff3c0
Souleymane MATO <smato@peopleinput.com>
2016-07-24 23:07:14 +0000

    add static resolve in dev

--- django_bookmarks/urls.py
+++ django_bookmarks/urls.py
@@ -1,13 +1,15 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 from bookmarks.views import *
+import os
+site_media = os.path.join(
+        os.path.dirname(__file__), 'static'
+)
 urlpatterns = [
-    # Examples:
-    # url(r'^$', 'django_bookmarks.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', main_page),
-    url(r'^dashboard$', main_page1),
+    url(r'^$|^dashboard$', main_page2),
     url(r'^user/(\w+)/$', user_page),
+    url(r'^login/$', 'django.contrib.auth.views.login'),
+    url(r'^logout/$', logout_page),
+    url(r'^site_media/(?P<path>.*)$', 'django.views.static.serve',{ 'document_root': site_media }),
 ]
