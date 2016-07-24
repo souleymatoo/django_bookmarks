28c25d5de8f75cf687651c563964287fb435b5bd
Souleymane MATO <smato@peopleinput.com>
2016-07-24 23:07:44 +0000

    updating

--- bookmarks/views.py
+++ bookmarks/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render
-from django.http import HttpResponse
 from django.template import Context
 from django.template.loader import get_template
 from django.http import HttpResponse, Http404
 from django.contrib.auth.models import User
-# Create your views here.
+from django.shortcuts import render_to_response
+from django.http import HttpResponseRedirect
+from django.contrib.auth import logout
 
 def main_page(request):
     output = '''
@@ -26,10 +26,21 @@ def main_page1(request):
     variables = Context({
         'head_title': 'Django Bookmarks',
         'page_title': 'Welcome to Django Bookmarks',
-        'page_body': 'Where you can store and share bookmarks!'
+        'page_body': 'Where you can store and share bookmarks!',
+        'user': request.user
     })
     output = template.render(variables)
     return HttpResponse(output)
+
+def main_page2(request):
+
+        return render_to_response(
+                'main_page.html',
+                {
+                    'user': request.user,
+                    'fullname' : request.user.get_full_name() if request.user.username else ''
+                  }
+        )
 
 def user_page(request, username):
     try:
@@ -38,9 +49,21 @@ def user_page(request, username):
         raise Http404('Requested user not found.')
     bookmarks = user.bookmark_set.all()
     template = get_template('user_page.html')
+
     variables = Context({
         'username': username,
+        'firstname': user.first_name,
+        'lastname': user.last_name,
         'bookmarks': bookmarks
     })
     output = template.render(variables)
     return HttpResponse(output)
+
+
+def logout_page(request):
+
+    request.session.flush()
+
+    logout(request)
+
+    return HttpResponseRedirect('/login')
